64528835d2c0a69e48e3b9c05aa1371ae62ab1f8
sunwoo020 <sunwoo020@naver.com>
2023-04-07 16:43:25 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoCreate from './Components/Todo-Create';
+import TodoHead from './Components/Todo-Head';
+import TodoList from './Components/Todo-List';
+import TodoTemplate from './Components/Todo-Template';
+import { TodoProvider } from './Components/Todo-Context';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+      
+    </TodoProvider>
+
+    
   );
 }
 
